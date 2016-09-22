0ecaaa1b0ecca2ea73f45899c391f96036aef4ab
rdash <rdash@localhost.localdomain>
2016-09-22 15:27:28 -0300

    asuka trash

--- event_listener/event_listener.ino
+++ event_listener/event_listener.ino
@@ -1,17 +1,84 @@
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
+int m13 = 13;
+int m12 = 12;
+int m11 = 11;
+int m10 = 10;
+
+int m09 = 9;
+int m08 = 8;
+int m07 = 7;
+int m06 = 6;
+
+int time01 = 10;
+int time00 = 0;
+
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
+  pinMode(m13, OUTPUT); 
+  pinMode(m12, OUTPUT);
+  pinMode(m11, OUTPUT); 
+  pinMode(m10, OUTPUT); 
+  
+  pinMode(m09, OUTPUT); 
+  pinMode(m08, OUTPUT); 
+  pinMode(m07, OUTPUT); 
+  pinMode(m06, OUTPUT); 
+  
 }
 
 void loop() {
   // print the string when a newline arrives:
   if (stringComplete) {
-    Serial.println(inputString);
+    if(inputString.startsWith("x")){
+      //Serial.println(inputString);
+        digitalWrite(m13, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m13, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m12, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m12, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m11, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m11, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m10, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m10, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+    }
+    else if(inputString.startsWith("y")){
+      //Serial.println(inputString);
+        digitalWrite(m09, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m09, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m08, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m08, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m07, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m07, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+        
+        digitalWrite(m06, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(time01);               // wait for a second
+        digitalWrite(m06, LOW);    // turn the LED off by making the voltage LOW
+        delay(time00);               // wait for a second
+    }
+    delay(50);
     // clear the string:
     inputString = "";
     stringComplete = false;
--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+"""
+http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#Python
+def get_line(start, end):
+"""
+
+
 import subprocess as sub
 import serial
 
@@ -9,7 +15,74 @@ def get_serial_port():
 	out, err = p.communicate()
 	return '/dev/'+out.strip()
 
-if __name__=='__main__':
+
+def hotwillies(ser,string):
+	ser.write(string+'|')
+
+def draw_line(x1,y1,x2,y2):
+
+    
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Determine how steep the line is
+    is_steep = abs(dy) > abs(dx)
+ 
+    # Rotate line
+    if is_steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+ 
+    # Swap start and end points if necessary and store swap state
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+ 
+    # Recalculate differentials
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Calculate error
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+ 
+    # Iterate over bounding box generating points between start and end
+    y = y1
+    points = []
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if is_steep else (x, y)
+        points.append(coord)
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+ 
+    # Reverse the list if the coordinates were swapped
+    if swapped:
+        points.reverse()
+    return points
+	
+def draw(ser,t):
+	x = 0
+	y = 0
+	for x1,y1 in t:
+		if(x1-x != 0):
+			hotwillies(ser,"x")
+		if(y1-y != 0):
+			hotwillies(ser,"y")
+		
+		x = x1
+		y = y1
+
+def init():
 	port = get_serial_port()
 	ser = serial.Serial(port, 9600)
-	ser.write('`Hello|')
+	return ser
+
+
+if __name__=='__main__':
+	ser = init()
+	p = draw_line(100, 100, 200, 400)
+	draw(ser, p)
