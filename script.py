81583cf14a5003fe2bc707dd5491feb57ad97b9c
rdash <rdash@localhost.localdomain>
2016-10-06 16:37:45 -0300

    Reimplemented Bresenham's algorithms

--- script.py
+++ script.py
@@ -13,50 +13,96 @@ def serial_write(ser, string):
 	print string
 	ser.write(string + '\n')
 
+# Plotting algorithms from: http://members.chello.at/easyfilter/bresenham.html
 
-# Based on: http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#Python
-def draw_line(x1, y1, x2, y2):
-    dx = x2 - x1
-    dy = y2 - y1
+def draw_circle(xm, ym, r):
+	x = - r
+	y = 0
+	err = 2 - 2 * r
+	points = []
+	while True:
+		points.append((xm - x, ym + y)) # 1st quadrant
+		points.append((xm - y, ym - x)) # 2nd quadrant
+		points.append((xm + x, ym - y)) # 3rd quadrant
+		points.append((xm + y, ym + x)) # 4th quadrant
+		r = err
+		if(r <= y):
+			y += 1
+			err += y * 2 + 1
+		if(r > x or err > y):
+			x += 1
+			err += x * 2 + 1
+		if(x >= 0):
+			return points
 
-    # Determine how steep the line is
-    is_steep = abs(dy) > abs(dx)
+def draw_ellipse(xm, ym, a, b):
+	x0 = xm - a
+	x1 = xm + a
+	y0 = ym - b
+	y1 = ym + b
+	b1 = b & 1
+	dx = 4 * (1 - a) * b * b
+	dy = 4 * (b1 + 1) * a * a
+	err = dx + dy + b1 * a * a
+	e2 = 0 # Dumb value
+	#""" Starting pixels
+	y0 += (b + 1) / 2
+	y1 = y0 - b1
+	#"""	
+	#"""
+	a *= 8 * a
+	b1 = 8 * b * b
+	#"""
+	points = []
+	while True:
+		points.append((x1, y0)) # 1st quadrant
+		points.append((x0, y0)) # 2nd quadrant
+		points.append((x0, y1)) # 3rd quadrant
+		points.append((x1, y1)) # 4th quadrant
+		e2 = 2 * err
+		if e2 <= dy:
+			y0 += 1
+			y1 -= 1
+			#err += dy += a
+			dy += a
+			err += dy
+		if e2 >= dx or 2 * err > dy:
+			x0 += 1
+			x1 -= 1
+			#err += dx += b1
+			dx += b1
+			err += dx
+		if x0 > x1:
+			break
+		
+	while y0 - y1 < b: # In too early break
+		points.append((x0 - 1, y0))
+		points.append((x1 + 1, y0))
+		y0 += 1
+		points.append((x0 - 1, y1))
+		points.append((x1 + 1, y1))
+		y1 -= 1
+	return points		
 
-    # Rotate line
-    if is_steep:
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-
-    # Swap start and end points if necessary and store swap state
-    swapped = False
-    if x1 > x2:
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-        swapped = True
-
-    # Recalculate differentials
-    dx = x2 - x1
-    dy = y2 - y1
-
-    # Calculate error
-    error = int(dx / 2.0)
-    ystep = 1 if y1 < y2 else -1
-
-    # Iterate over bounding box generating points between start and end
-    y = y1
-    points = []
-    for x in range(x1, x2 + 1):
-        coord = (y, x) if is_steep else (x, y)
-        points.append(coord)
-        error -= abs(dy)
-        if error < 0:
-            y += ystep
-            error += dx
-
-    # Reverse the list if the coordinates were swapped
-    if swapped:
-        points.reverse()
-    return points
+def draw_line(x0, y0, x1, y1):
+	dx = abs(x1 - x0)
+	sx = 1 if x0 < x1 else -1
+	dy = -abs(y1 - y0)
+	sy = 1 if y0 < y1 else -1
+	err = dx + dy
+	e2 = 0 # Dumb value
+	points = []
+	while True:
+		points.append((x0, y0))
+		if x0 == x1 or y0 == y1:
+			return points
+		e2 = 2 * err
+		if e2 >= dy:
+			err += dy
+			x0 += sx
+		if e2 <= dx:
+			err += dx
+			y0 += sy	
 
 def draw(ser, t):
 	import time
